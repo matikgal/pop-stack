4acf76cf99ab1baaae74e12510bc0831decb72cb
Mateusz <77492310+matikgal@users.noreply.github.com>
2025-12-10 17:25:55 +0100

    chore: migrate to pop-stack and configure gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/pop-stack/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
